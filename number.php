48ff748c9ed34e9590ea82234af314db2dfd1c23
Henryc Liu <liu1734@gmail.com>
2023-09-21 17:36:23 +0800

    隨機數字以及顯示出來

--- number.php
+++ number.php
@@ -8,14 +8,22 @@
 <body>
 
 <?php
-
-
-
-$Number = rand(0,9);
+session start();
+/*$Number = rand(0,9);
 while(count($Number)<3) {
     print_r($Number);
-}
+}*/
 
+$Number = array();
+
+while (count($Number) < 3) { //當$Number小於三個
+    $RandomNumber = rand(0, 9);  //隨機跑出數字
+    
+    if (!in_array($RandomNumber, $Number)) { //確認$RandomNumber數字是否有重複
+        $Number[] = $RandomNumber;  //沒有就丟回陣列
+    }
+}
+print_r($Number); //讓數字排整齊
 
 
 
