34e65f3e692e1a3506cd354dc0c34307f51680dc
Henryc Liu <liu1734@gmail.com>
2023-09-21 17:32:02 +0800

    第一次嘗試不重複數字

---
+++ number.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number</title>
+</head>
+<body>
+
+<?php
+
+
+
+$Number = rand(0,9);
+while(count($Number)<3) {
+    print_r($Number);
+}
+
+
+
+
+?>
+
+
+
+
+
+</body>
+</html>
